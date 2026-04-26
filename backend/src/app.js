d5d3239fd762d535e0bea4fe2e28446b8fdf8be0
Yago Delfino <daventhidas@gmail.com>
2026-04-26 12:27:50 -0300

    feat: adiciona suporte a CORS no servidor e atualiza dependências

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const { authentication, login } = require("./services/authService");
+const cors = require("cors");
 const childrenRoutes = require("./routes/children.routes");
 const authRoutes = require("./routes/auth.routes");
 
 function createApp() {
   const app = express();
+  const allowedOrigins = (
+    process.env.FRONTEND_URL
+  )
+
+  app.use(
+    cors({
+      origin: allowedOrigins,
+      methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
   app.use(express.json());
 
   app.use("/auth", authRoutes);
