becaf7b467cd79dbdef5f73ac0d797bd9dee9935
Yago Delfino <daventhidas@gmail.com>
2026-04-26 10:11:13 -0300

    feat: adiciona fontes Cera Compact Pro e atualiza layout para usar nova fonte, adição das cores de acordo com o padrão de identidade visual da prefeitura

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const ceraCompact = localFont({
+  src: [
+    {
+      path: "./fonts/Cera Compact Pro Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Cera Compact Pro Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Cera Compact Pro Black.otf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-cera-compact",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,10 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="pt-Br"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="pt-BR" className={`${ceraCompact.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
